4d018703b7bcae97384ef404a788ee6ba5079127
ktyouta <ktyouta0506@gmail.com>
2025-04-27 22:51:30 +0900

    共通エラーハンドリング処理の実装

---
+++ src/Exception/GlobalException.ts
@@ -0,0 +1,31 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { Logger } from 'src/common/logger/Logger';
+
+@Catch()
+export class GlobalException implements ExceptionFilter {
+
+    private static readonly _HTTP_STATUS_ERROR = 500;
+
+    catch(exception: Error, host: ArgumentsHost) {
+
+        const httpError = host.switchToHttp();
+        const response = httpError.getResponse<Response>();
+        const request = httpError.getRequest<Request>();
+
+        const message = exception.message;
+        const status = GlobalException._HTTP_STATUS_ERROR;
+
+        console.log(`Error occurred: ${message}`);
+
+        // エラーログに出力
+        Logger.error(message);
+
+        response.status(status).json({
+            statusCode: status,
+            timestamp: new Date().toISOString(),
+            path: request.url,
+            message: `予期しないエラーが発生しました`,
+        });
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import ENV from '../env.json';
+import { GlobalException } from './Exception/GlobalException';
 
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
 
   // CORS設定
   app.enableCors({
     origin: [`${ENV.CORS.PROTOCOL}${ENV.CORS.DOMAIN}${ENV.CORS.PORT}`],
   });
+
+  // エラーハンドリング
+  app.useGlobalFilters(new GlobalException());
 
   await app.listen(ENV.PORT);
 }
